6ef635345cb8106f70ffe1cdb05c5e6036c93581
Hassan ben haj <bussinesbhb@gmail.com>
2017-02-23 14:05:45 +0000

    initial commit

---
+++ controller/convert.js
@@ -0,0 +1,34 @@
+module.exports = {
+	 dateToTimestamp: function(date){
+		let myDate;
+		let newDate;
+		//in case the date hase dashes in it 
+		if (date.includes('-')) {
+			myDate = date.split('-');
+		}else{
+			myDate = date.split(' ');
+		}
+		
+		newDate = myDate[0]+","+myDate[1]+","+myDate[2];
+		return new Date(newDate).getTime();
+	},
+
+ 	dateToNaturalDate: function(date){
+		var monthNames = ["January", "February", "March", "April", "May", "June",
+	  		"July", "August", "September", "October", "November", "December"];
+		let d = String;
+
+		if (date.includes('-')) {
+			d = date.split('-');
+		}else{
+			d = date.split(' ');
+		}
+		
+		if (!Number.isInteger(d[0])) {
+			return monthNames[d[0]-1]+' '+d[1]+', '+d[2];
+		}else{
+			return d[0]+' '+d[1]+' '+d[2];
+		}
+		
+	}
+};
--- server.js
+++ server.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const convert = require('./controller/convert.js');
 const app = express();
+//template engine
+app.set('view engine', 'ejs');
+//middleware
+app.use(express.static("./public"));
 
 app.get('/', function(req, res){
-	res.send('Hello World!');
-});
+	res.render('home');
+})
+
+
+app.get('/:time', function(req, res){
+	const regex = /\d/g;
+	//if it's text and numbers (eg: january 20, 2000) 
+	//problem to fix: it passes stuff like this (eg: azdfcdsc22)
+	if (isNaN(req.params.time) && regex.test(req.params.time)) {
+		//convert to timestamp and set date to natural language form
+		res.render("Api", {unix: convert.dateToTimestamp(req.params.time)/1e3,
+					   	   	   natural: convert.dateToNaturalDate(req.params.time)
+					  	      });
+	}else if(!isNaN(req.params.time)){
+		//if it's a number then it's a timestamp
+		res.render("Api", {unix: req.params.time,
+						//converts timestamp to natural language date
+					   	   natural: new Date(Number(req.params.time)*1000).toDateString()
+					  	  });	
+	}else {
+		res.render("Api", {unix: 'null',
+					   	   natural: 'null'
+					  	  });
+	}
+})
 
 app.listen(3000, function(){
 	console.log('Server running on port 3000');
